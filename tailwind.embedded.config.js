9c59cb95020ab4111d5863fb5fe9da044db2301a
M.K. Safi <msafi@msafi.com>
2022-05-26 15:01:29 -0700

    Add embedded styles

---
+++ tailwind.embedded.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ['./common/classes', './common/input-embedded.scss'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  corePlugins: {
+    backgroundOpacity: false,
+    borderOpacity: false,
+    textOpacity: false,
+    preflight: false,
+  },
+};
